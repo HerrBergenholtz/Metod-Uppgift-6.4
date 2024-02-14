196427500160b3681be80d28afdc2838012be05d
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-14 10:59:42 +0100

    Add project files.

---
+++ Metod-Uppgift-6.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_6_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Välkommen till potensräkning");
+            Console.WriteLine("Vilken bas vill du räkna?");
+            float bas = float.Parse(Console.ReadLine());
+            Console.WriteLine("Vilken exponent vill du exponera med?");
+            float exponent = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Resultatet blir: {Exponera(bas, exponent)}");
+        }
+
+        static double Exponera(float bas, float exponent)
+        {
+            return Math.Pow(bas, exponent);
+        }
+    }
+}
